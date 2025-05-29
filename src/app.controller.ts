ef7d688a03087318cb1847ec5acc28f3cc79af97
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-05-29 20:58:02 +0300

    feat: set test endpoint with mock data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('certificate')
+  getCertificate() {
+    return {
+      recipientName: 'Charlotte Smith',
+      eventName: 'Local Chess Tournament 2025',
+      issueDate: 'May 29, 2025',
+    };
   }
 }
