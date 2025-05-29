f1c1da5c09021c244375ead04f46d8c771b078ec
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-05-29 22:05:29 +0300

    feat: map generate certificate pdf service method to a test endpoint with hard-coded parameters

--- src/pdf/pdf.controller.ts
+++ src/pdf/pdf.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { PdfService } from './pdf.service';
+import { Response } from 'express';
 
 @Controller('pdf')
-export class PdfController {}
+export class PdfController {
+  constructor(private readonly pdfService: PdfService) {}
+
+  @Get('test')
+  async getPdf(@Res() res: Response) {
+    const pdfBuffer = await this.pdfService.generateCertificatePdf({
+      recipientName: 'Charlotte Smith',
+      eventName: 'Local Chess Tournament 2025',
+      issueDate: 'May 29, 2025',
+    });
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename="certificate.pdf"',
+    });
+
+    res.send(pdfBuffer);
+  }
+}
