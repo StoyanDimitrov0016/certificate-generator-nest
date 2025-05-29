0ec68f75d3a3bf42917d500f8f210d55b5525d6c
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-05-29 22:04:57 +0300

    feat: compile the static handlebars template and create pdf with puppeteer

--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import handlebars from 'handlebars';
+import * as path from 'path';
+
+import * as puppeteer from 'puppeteer';
 
 @Injectable()
-export class PdfService {}
+export class PdfService {
+  async generateCertificatePdf(data: any): Promise<Buffer> {
+    const templatePath = path.join(__dirname, '../../views/certificate.hbs');
+    const templateSource = fs.readFileSync(templatePath, 'utf-8');
+
+    const template = handlebars.compile(templateSource);
+    const html = template(data);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+
+    const pdfUint8Array = await page.pdf({ format: 'A4' });
+    const pdfBuffer = Buffer.from(pdfUint8Array);
+
+    await browser.close();
+    return pdfBuffer;
+  }
+}
