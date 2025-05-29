bc6b504fc3beb4f1dcb19f089bcbef9a316f797d
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-05-29 23:24:02 +0300

    feat: add certificate dto type as a receiving parameter in service method, add bg flag to puppeteers config and return parsed pdf buffer

--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -1,27 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
-import handlebars from 'handlebars';
 import * as path from 'path';
-
-import * as puppeteer from 'puppeteer';
+import handlebars from 'handlebars';
+import puppeteer from 'puppeteer';
+import { CertificateDto } from './dtos/certificate.dto';
 
 @Injectable()
 export class PdfService {
-  async generateCertificatePdf(data: any): Promise<Buffer> {
+  async generateCertificatePdf(data: CertificateDto): Promise<Buffer> {
     const templatePath = path.join(__dirname, '../../views/certificate.hbs');
     const templateSource = fs.readFileSync(templatePath, 'utf-8');
 
-    const template = handlebars.compile(templateSource);
-    const html = template(data);
+    const templateHandler = handlebars.compile(templateSource);
+    const html = templateHandler(data);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
 
-    const pdfUint8Array = await page.pdf({ format: 'A4' });
-    const pdfBuffer = Buffer.from(pdfUint8Array);
+    const pdfUint8Array = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+    });
 
     await browser.close();
+
+    const pdfBuffer = Buffer.from(pdfUint8Array);
     return pdfBuffer;
   }
 }
