808b908511713ba39987de0f8f5e000051c8b531
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-06-07 22:17:48 +0300

    feat: add theme as a certificate DTO property that depends on the constant theme enum

--- src/pdf/dtos/certificate.dto.ts
+++ src/pdf/dtos/certificate.dto.ts
@@ -1,36 +1,54 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { TemplateTheme, TEMPLATE_THEMES } from '../../constants';
 
 export class CertificateDto {
   @IsString()
   @IsNotEmpty()
+  @IsEnum(TemplateTheme, {
+    message: `theme must be one of the following values: ${TEMPLATE_THEMES.join(', ')}`,
+  })
+  theme: TemplateTheme;
+
+  @IsString()
+  @IsNotEmpty()
   organizationName: string;
+
   @IsString()
   @IsNotEmpty()
   certificateTitle: string;
+
   @IsString()
   @IsNotEmpty()
   certificateSubtitle: string;
+
   @IsString()
   @IsNotEmpty()
   presentationText: string;
+
   @IsString()
   @IsNotEmpty()
   recipientName: string;
+
   @IsString()
   @IsNotEmpty()
   achievementText: string;
+
   @IsString()
   @IsNotEmpty()
   courseTitle: string;
+
   @IsString()
   @IsNotEmpty()
   completionDate: string;
+
   @IsString()
   @IsNotEmpty()
   signatureName: string;
+
   @IsString()
   @IsNotEmpty()
   signatureLabel: string;
+
   @IsString()
   @IsNotEmpty()
   issueDate: string;
