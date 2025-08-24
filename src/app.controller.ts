65fdc32a86338a7102fcd0e5bf439d9e5c875925
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-08-24 21:59:00 +0300

    add "greeting-hello", "ping-pong", and "health-check" mostly for fun to the app service and controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,32 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { join } from 'path';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  rootPath: string = join(__dirname, '..', 'public');
+  htmlName: string = 'index.html';
+
+  constructor(private readonly appService: AppService) {}
+
   @Get()
-  async redirectToStatic(@Res() res: Response) {
-    res.sendFile(join(__dirname, '..', '..', 'public', 'index.html'));
+  root(@Res() res: Response) {
+    return res.sendFile(this.htmlName, { root: this.rootPath });
+  }
+
+  @Get('hello')
+  getHello() {
+    return this.appService.getHello();
+  }
+
+  @Get('ping')
+  getPong() {
+    return this.appService.getPong();
+  }
+
+  @Get('health')
+  getHealth() {
+    return this.appService.getHealth();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getPong(): string {
+    return 'Pong!';
+  }
+
+  getHealth(): string {
+    return 'Healthy!';
+  }
 }
