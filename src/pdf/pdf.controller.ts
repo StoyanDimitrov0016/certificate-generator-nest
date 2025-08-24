475316d52c172122f1f72ae61fe2189f1db8e1a1
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-08-24 21:58:07 +0300

    refactor: rename generateCertificatePdf to createCertificate and create a dedicated private method for getting the filename based on dto data

--- src/pdf/pdf.controller.ts
+++ src/pdf/pdf.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, Header } from '@nestjs/common';
 import { StreamableFile } from '@nestjs/common';
 import { PdfService } from './pdf.service';
 import { CertificateDto } from './dtos/certificate.dto';
+import { TemplateTheme } from 'src/constants';
 
 @Controller('pdf')
 export class PdfController {
@@ -9,18 +10,22 @@ export class PdfController {
 
   @Post('generate')
   @Header('Content-Type', 'application/pdf')
-  async create(@Body() data: CertificateDto): Promise<StreamableFile> {
-    const pdfBuffer = await this.pdfService.generateCertificatePdf(data);
+  async create(
+    @Body() certificateDto: CertificateDto,
+  ): Promise<StreamableFile> {
+    const pdfBuffer = await this.pdfService.createCertificate(certificateDto);
 
-    const name = data.recipientName
-      .slice(0, 30)
-      .toLowerCase()
-      .replace(/\s+/g, '_');
-    const filename = `certificate_${name}_${data.theme}.pdf`;
+    const { recipientName, theme } = certificateDto;
+    const filename = this.getFilename(recipientName, theme);
 
     return new StreamableFile(pdfBuffer, {
       type: 'application/pdf',
       disposition: `attachment; filename="${filename}"`,
     });
+  }
+
+  private getFilename(recipientName: string, theme: TemplateTheme) {
+    const name = recipientName.slice(0, 30).toLowerCase().replace(/\s+/g, '_');
+    return `certificate_${name}_${theme}.pdf`;
   }
 }
